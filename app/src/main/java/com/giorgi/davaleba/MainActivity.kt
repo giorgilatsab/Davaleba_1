c10a893826b31034b8ee1866868203aa4e49478d
giorglatsab <giorgilatsabidze84@gmail.com>
2022-06-16 00:46:53 +0400

    Initial commit

---
+++ app/src/main/java/com/giorgi/davaleba/MainActivity.kt
@@ -0,0 +1,58 @@
+package com.giorgi.davaleba
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+
+
+//        fun main() {
+//            sum()
+//            answer()
+//        }
+//
+//        fun sum(){
+//
+//            println("დაწყება")
+//
+//            try {
+//                println("შეიყვანეთ x ცვლადის მნიშვლენობა")
+//                val x = readLine()!!.toFloat()
+//                println("შეიყვანეთ y ცვლადის მნიშვლენობა")
+//                val y = readLine()!!.toFloat()
+//                println("x და y-ის ჯამი არის: ${x + y}")
+//            }
+//            catch (e: Exception) {
+//                println("შეიყვანეთ რიცხვი")
+//            }
+//        }
+//        fun answer(){
+//            println()
+//            println("გსურთ პროგრამის ხელახლა დაწყება? Y/N ?")
+//
+//            val answer = readLine()!!
+//
+//
+//            while (answer.equals("Y" )){
+//                main()
+//            }
+//
+//            while (answer.equals("N")){
+//                break
+//            }
+//            while (answer!="N" && answer!="Y"){
+//                break
+//            }
+//        }
+
+
+
+
+    }
+}
